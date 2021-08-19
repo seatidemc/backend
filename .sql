abf2264bd0f78da580e1bbdfd7bc40424cc83d7a
Subilan <suyuanliang1995@gmail.com>
2021-08-19 15:48:21 +0800

    (db) add table `cmd_history`

--- .sql
+++ .sql
@@ -12,4 +12,11 @@ CREATE TABLE history (
     `instance` VARCHAR(50),
     `action` VARCHAR(10) NOT NULL
 );
+CREATE TABLE cmd_history (
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `created_at` DATETIME NOT NULL,
+    `created_by` VARCHAR(20) NOT NULL,
+    `command_id` VARCHAR(50) NOT NULL,
+    `invocation_id` VARCHAR(50) NOT NULL
+);
 INSERT INTO ecs_status () VALUES ();
