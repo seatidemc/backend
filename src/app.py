e8450b113d02882f40126bc6749e00e2767b7866
Subilan <suyuanliang1995@gmail.com>
2021-08-23 17:39:02 +0800

    fix: debug

--- src/app.py
+++ src/app.py
@@ -70,4 +70,4 @@ if __name__ == '__main__':
         from waitress import serve
         serve(app, host='0.0.0.0', port='8080') 
     else:
-        app.run(debug=False)
+        app.run(debug=True)
